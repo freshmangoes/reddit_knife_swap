6beff5aca2b19d14a7f61b21cc673b75f3c2cb03
Kyle Cilia <kylecilia@icloud.com>
2020-09-05 21:17:47 -0700

    Incorporates link into title and user profile link as username

--- client/src/Components/ListingBox/ListingBox.js
+++ client/src/Components/ListingBox/ListingBox.js
@@ -4,20 +4,29 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const ListingBox = (props) => {
-	let { title, text, images, username, link, displayStatus, data_status } = props;
+	let {
+		title,
+		text,
+		images,
+		username,
+		link,
+		displayStatus,
+		data_status,
+	} = props;
 
 	return (
 		<div data_status={data_status}>
-			<h1>{title} - {displayStatus}</h1>
-			<h5>{username}</h5>
+			<h1>
+				<a href={link}>{title}</a> - {displayStatus}
+			</h1>
+			<h5>
+				Posted by <a href={`https://reddit.com/u/${username}`}>{username}</a>
+			</h5>
 			<div>
-        <ReactMarkdown source={text}/>
-      </div>
+				<ReactMarkdown source={text} />
+			</div>
 			<p>{images}</p>
-			<button>
-				<a href={link}>Link</a>
-			</button>
-			<hr/>
+			<hr />
 		</div>
 	);
 };
